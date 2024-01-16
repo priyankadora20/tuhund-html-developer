785fb3750313a8ec5939158a1087a18aef4d928b
priyanka dora <prdora97@gmail.com>
2024-01-16 22:06:51 +0530

    tuhund - test

--- Tuhund test folder/navbar/navbar.js
+++ Tuhund test folder/navbar/navbar.js
@@ -1,8 +1,1 @@
-  window.addEventListener("scroll", function () {
-        const nav = document.getElementById("navbar");
-        if (window.scrollY > 50) {
-            nav.classList.add("transparent");
-        } else {
-            nav.classList.remove("transparent");
-        }
-    });
+  
